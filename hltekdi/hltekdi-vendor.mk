ef47939f2006a8fd004f45e34a31355bb3d261c4
sakuraba <sakura.bar.com@gmail.com>
2014-09-05 12:34:41 +0900

    update proprietary hltekdi

--- hltekdi/hltekdi-vendor.mk
+++ hltekdi/hltekdi-vendor.mk
@@ -16,8 +16,6 @@
 
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \
-    libtime_genoff \
-    com.qualcomm.location \
-    PPPreference
+    com.qualcomm.location
 
 $(call inherit-product, vendor/samsung/hltekdi/hltekdi-vendor-blobs.mk)
